26f94a6ed8ece82ff496c5e2140c34178435c1e5
george <gkephart@cnm.edu>
2019-06-11 20:20:08 -0600

    added font awesome icons onto the map

--- app/src/index.js
+++ app/src/index.js
@@ -1,29 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import {GoogleMap, Marker, withGoogleMap, withScriptjs} from "react-google-maps";
+import ReactMapboxGl, {Layer, Feature, Marker, Popup} from "react-mapbox-gl";
+import {library} from '@fortawesome/fontawesome-svg-core'
+	;
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEnvelope, faMapMarker} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faMapMarker);
 
 
-const App = withScriptjs(withGoogleMap(() =>
-	<GoogleMap
-		defaultZoom={8}
-		defaultCenter={{lat: 35.0856197, lng: -106.64924}}
-	>
-	 <Marker
-		 position={{lat: 34.397, lng: -106.7}}
-	 />
-	</GoogleMap>
-));
+const App = () => {
 
 
-ReactDOM.render(
-	<App
-	googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCp8aJ6-c7UwVvh-tVeXsWQ3nYQrQ4c3r4"
-	loadingElement={<div style={{height: `100%`}}/>}
-	containerElement={<div style={{height: `400px`}}/>}
-	mapElement={<div style={{height: `100%`}}/>}
+	const [points, setPoints] = useState([
+		{lat: 35.332, lng: -106.652},
+		{lat: 35.339, lng: -106.656},
+		{lat: 35.40, lng: -106.666},
+		{lat: 35.23, lng: -106.4444}
+	]);
 
 
-/>, document.getElementById('root'));
+	const Map = ReactMapboxGl({
+		accessToken: "pk.eyJ1IjoiZ2Vvcmdla2VwaGFydCIsImEiOiJjanQ4cmdmYjkwYnZnNDNwNDF4NXFiMTJmIn0.MwDDiyszR0QFmMYMNvzi1Q"
+	});
+
+	return (
+		<>
+			<Map
+				style="mapbox://styles/mapbox/streets-v9"
+				containerStyle={{
+					height: "100vh",
+					width: "100vw"
+				}}
+				center={[-106.65, 35.33]}
+			>
+				{points.map(point => (
+					<Marker key={point.lat} coordinates={[point.lng, point.lat]} anchor="bottom"><FontAwesomeIcon
+						icon="map-marker" size="lg"/>
+					</Marker>
+				))};
+			</Map>
+
+		</>
+	)
+};
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
